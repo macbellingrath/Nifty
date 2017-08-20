94f1695d46336a84ea7cb28ee763a62fe463461c
felix91gr <felix91gr@gmail.com>
2017-08-20 20:08:54 -0300

    Radically changed the design of the Protocol and the default extension. Now it's much more realistic and Swifty

--- Sources/TensorProtocol.swift
+++ Sources/TensorProtocol.swift
@@ -22,153 +22,75 @@
 
 import Foundation
 
-
+// TODO for Swift 4, add Codable conformance.
 /// Data structure for an N-D, row-major order array.
 public protocol TensorProtocol: CustomStringConvertible
 {   
-    //TODO for Swift 4, replace the associatedtype line with this one.
+    // TODO for Swift 4, replace the associatedtype line with this one.
     // associatedtype Element : Numeric
     associatedtype Element    
 
     /// Number of elements in the tensor.
-    var count: Int { get set }
+    var count: Int { get }
 
+    /// Internal property. Set at your own risk
+    var _size: [Int] { get set }
     /// Number of elements in each dimension of the tensor.
-    var size: [Int] { get set }
+    var size:  [Int] { get }
 
-    // FIXME: this might not be correct.
-    // Should only be able to change the members, and not the 
-    // Array itself.
-    var data: [Element] { get set }
-
-    /// Optional name of tensor (e.g., for use in display).
-    var name: String? { get set }
-
-    /// Determine whether to show name when displaying tensor.
-    var showName: Bool { get set }
-
-    /// Formatter to be used in displaying tensor elements.
-    var format: NumberFormatter { get set }
-
-    // FIXME: this is a hack to allow for a default constructor.
-    // Maybe we can avoid it?
-    init()
-
-    /// Initialize a new tensor of the given size from a row-major ordered array of data.
-    init(_ size: [Int], _ data: [Element], name: String?, showName: Bool?)
-
-    /// Initialize a new tensor of the given size and uniform value.
-    init(_ size: [Int], value: Element, name: String?, showName: Bool?)
-
-
-    /// Initialize a new tensor from the data in a given tensor.
-    ///
-    /// - Parameters:
-    ///    - tensor: tensor to initialize from
-    ///    - name: optional name of new tensor
-    ///    - showName: determine whether to print the tensor name; defaults to true if the tensor is
-    ///        given a name, otherwise to false
-    init(_ tensorP: Self, name: String?, showName: Bool?)
+    /// Internal property. Set at your own risk
+    var _data: [Element] { get set }
+    /// Data contained in tensor in row-major order.
+    var data:  [Element] { get  }
     
-    /// Access a single element of the tensor with a subscript.
-    ///
-    /// If only one subscript is given, it is interpretted as a row-major order linear index. 
-    /// Otherwise, the given subscripts are treated as indexes into each dimension.
-    ///
-    /// - Parameters:
-    ///    - s: subscripts
-    /// - Returns: single value at index
+    // TODO: check if these are implementable for every TensorProtocol type
+    // Even though they are basic initializers, they might not be general enough.
+    // init(_ size: [Int], _ data: [Element], name: String? = nil, showName: Bool? = nil)
+    // init(_ size: [Int], value: T, name: String? = nil, showName: Bool? = nil)
+    // init(_ size: [Int], value: Element, name: String?, showName: Bool?)
+    
+    // This is a copy initializer. The only truly general initializer, I think.
+    init(_ otherTP: Self, name: String?, showName: Bool?)
+    
+    // TODO for Swift 4, use the new indexing capabilities for the
+    // subscripting functions :)
     subscript(_ s: [Int]) -> Element { get set }
     
     subscript(_ s: Int...) -> Element { get set }
     
-    subscript(_ s: SliceIndex...) -> Self { get set }
+    // TODO: check if this is implementable for every TensorProtocol type
+    // subscript(_ s: SliceIndex...) -> Self { get set }
 }
 
 
 // Default implementations for the structs that adhere to the TensorProtocol
-extension TensorProtocol {
+public extension TensorProtocol {
 
-    /// Initialize a new tensor of the given size from a row-major ordered array of data.
-    ///
-    /// - Parameters:
-    ///    - size: number of elements in each dimension of the tensor
-    ///    - data: tensor data in row-major order
-    ///    - name: optional name of tensor
-    ///    - showName: determine whether to print the tensor name; defaults to true if the tensor is
-    ///        given a name, otherwise to false
-    public init(_ size: [Int], _ data: [Element], name: String? = nil, showName: Bool? = nil)
-    {       
-        self.init()
-        let n = size.reduce(1, *)
-
-        precondition(n > 0, "TensorProtocol must have at least 1 element")
-        precondition((size.filter({$0 <= 0})).count == 0, "TensorProtocol must have all dimensions > 0")
-        precondition(data.count == n, "TensorProtocol dimensions must match data")
-
-        self.size = size
-        self.count = n
-        self.data = data    
-        self.name = name    
-
-        if let show = showName
-        {
-            self.showName = show
-        }
-        else
-        {
-            self.showName = name != nil
-        }
-
-        // default display settings
-        let fmt = NumberFormatter()
-        fmt.numberStyle = .decimal
-        fmt.usesSignificantDigits = true
-        fmt.paddingCharacter = " "
-        fmt.paddingPosition = .afterSuffix
-        fmt.formatWidth = 8
-        self.format = fmt
+    // We compute count on the fly, because it's less redundant this way.
+    var count : Int {
+        return size.reduce(1, *)
     }
 
-    /// Initialize a new tensor of the given size and uniform value.
-    ///
-    /// - Parameters:
-    ///    - size: number of elements in each dimension of the tensor
-    ///    - value: single value repeated throughout tensor
-    ///    - name: optional name of tensor
-    ///    - showName: determine whether to print the tensor name; defaults to true if the tensor is
-    ///        given a name, otherwise to false
-    public init(_ size: [Int], value: Element, name: String? = nil, showName: Bool? = nil)
-    {
-        let n = size.reduce(1, *)
-        precondition(n > 0, "TensorProtocol must contain at least one element")
-        let data = Array<Element>(repeating: value, count: n)
-        self.init(size, data, name: name, showName: showName)
+    private(set) var size : [Int] { 
+        get {
+            return _size        
+        }
+        set {
+            _size = newValue
+        }
     }
 
-    /// Initialize a new tensor from the data in a given tensor.
-    ///
-    /// - Parameters:
-    ///    - tensor: tensor to initialize from
-    ///    - name: optional name of new tensor
-    ///    - showName: determine whether to print the tensor name; defaults to true if the tensor is
-    ///        given a name, otherwise to false
-    public init(_ tensor: Self, name: String? = nil, showName: Bool? = nil)
-    {
-        self.init(tensor.size, tensor.data, name: name, showName: showName)
+    private(set) var data : [Element] { 
+        get {
+            return _data        
+        }
+        set {
+            _data = newValue
+        }
+    }
 
-        // need to create new formatter instance, copying values
-        self.format = _copyNumberFormatter(tensor.format)
-    }    
-
-    /// Access a single element of the tensor with a subscript.
-    ///
-    /// If only one subscript is given, it is interpretted as a row-major order linear index. 
-    /// Otherwise, the given subscripts are treated as indexes into each dimension.
-    ///
-    /// - Parameters:
-    ///    - s: subscripts
-    /// - Returns: single value at index
+    // Subscripting can be implemented well in the extension! 
+    // Horray for DRY code :D
     public subscript(_ s: [Int]) -> Element
     {
         get
@@ -204,18 +126,19 @@ extension TensorProtocol {
     public subscript(_ s: Int...) -> Element
     {
         get { return self[s] }
-        set(newValue) { self[s] = newValue }
+        set (newValue) { self[s] = newValue }
     }
     
     private func getValue(index: Int) -> Element
     {
-
+        // FIXME: replace this precondition by an error
         precondition(index >= 0 && index < self.count, "TensorProtocol subscript out of bounds")
         return self.data[index]            
     }
 
     private mutating func setValue(index: Int, value: Element)
     {
+        // FIXME: replace this precondition by an error
         precondition(index >= 0 && index < self.count, "TensorProtocol subscript out of bounds")
         self.data[index] = value
     }
@@ -223,6 +146,7 @@ extension TensorProtocol {
     private func getValue(subscripts: [Int]) -> Element
     {
         let index = sub2ind(subscripts, size: self.size)
+        // FIXME: replace this precondition by an error
         precondition(index >= 0, "TensorProtocol subscript out of bounds")
         return self.data[index]
     }
@@ -230,64 +154,8 @@ extension TensorProtocol {
     private mutating func setValue(subscripts: [Int], value: Element)
     {
         let index = sub2ind(subscripts, size: self.size)
+        // FIXME: replace this precondition by an error
         precondition(index >= 0, "TensorProtocol subscript out of bounds")
         self.data[index] = value
     }   
-
-    /// Access a slice of the tensor with a subscript range.
-    ///
-    /// If only one range is given, it is interpretted as a row-major order linear index. Otherwise,
-    /// the given subscripts are treated as indexes into each dimension.
-    ///
-    /// - Parameters:
-    ///    - s: subscripts 
-    /// - Returns: new tensor composed of slice
-    public subscript(_ s: SliceIndex...) -> Self
-    {
-        get
-        {
-            // FIXME: implement
-            fatalError("Not yet implemented")
-
-            // assert(s.count > 0)
-
-            // if s.count == 1
-            // {
-            //     return self.getSlice(index: s[0])
-            // }
-            // else
-            // {
-            //     return self.getSlice(subscripts: s)
-            // }
-        }
-
-        set(newValue)
-        {    
-            // FIXME: implement
-            fatalError("Not yet implemented")
-            
-            // assert(s.count > 0)
-
-            // if s.count == 1
-            // {
-            //     self.setSlice(index: s[0], value: newValue)
-            // }
-            // else
-            // {
-            //     self.setSlice(subscripts: s, value: newValue)
-            // }
-
-        }
-    }
-    /// Return tensor contents in an easily readable grid format.
-    ///
-    /// - Note: The formatter associated with this tensor is used as a suggestion; elements may be
-    ///     formatted differently to improve readability. Elements that can't be displayed under the 
-    ///     current formatting constraints will be displayed as '#'.    
-    /// - Returns: string representation of tensor
-    public var description: String
-    {
-        fatalError("Not yet implemented")
-    }    
-
 }
